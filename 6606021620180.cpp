dabac0ce9efdee12fccb7b16decedc65cbc966bf
sasipatson <139094335+sasipatson@users.noreply.github.com>
2023-07-17 10:10:29 +0700

    Add files via upload

---
+++ 6606021620180.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+
+  string Name ;
+  int Year;
+  cout << " Enter name : ";
+  cin >> Name ;
+  cout << "Enter Year of birth : ";
+  cin >> Year ;
+  cout << "\nYour Age = " << 2566 - Year << " years old. " <<endl ;
+
+	  system("pause");
+  return 0;
+
+} 
