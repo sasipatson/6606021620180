1cbaaf456db421094528b57a90691b7a6c2cd3b4
sasipatson <139094335+sasipatson@users.noreply.github.com>
2023-08-06 19:38:26 +0700

    Add files via upload

--- 6606021620180.cpp
+++ 6606021620180.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
-int main()
+int main ()
 {
+	char input1,input2,input3;
+	int A = 10 ;
+	int B = 20 ;
+	int C = 30 ;
+	int T = 0 ;
+	int num1 = 0,num2 = 0,num3 = 0 ;
+	cout << "Enter Character(A,B,C) : ";
+	cin >> input1 ;
+	if (input1 == 'A'){
+		num1 += A ;
+	}else if (input1 == 'B'){
+		num1+= B ;
+	}else if (input1 == 'C'){
+		num1 += C ;
+	}
+	cout << "Enter Character (A,B,C) : ";
+	cin >> input2 ;
+	if (input2 == 'A'){
+		num2 += A ;
+	}else if(input2 == 'B'){
+		num2 += B ;
+	}else if(input2 == 'C'){
+		num2 += C ;
+	}
+	cout << "Enter Character (A,B,C) : ";
+	cin >> input3 ;
+	if (input3 == 'A'){
+		num3 += A ;
+	}else if(input3 == 'B'){
+		num3 += B ;
+	}else if(input3 == 'C'){
+		num3 += C ;
+	}
+T = num1+num2+num3 ;
+cout << input1 << " = " << num1 << " , " <<input2 << " = " << num2 << " , " << input3 << " = " << num3 <<endl;
+cout << input1 << " + " << input2 << " + " << input3 << " = " << T <<endl;
 
-  string Name ;
-  int Year;
-  cout << " Enter name : ";
-  cin >> Name ;
-  cout << "Enter Year of birth : ";
-  cin >> Year ;
-  cout << "\nYour Age = " << 2566 - Year << " years old. " <<endl ;
-
-	  system("pause");
-  return 0;
-
-} 
+return (0);
+}
